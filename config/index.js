a3c0bb17d4bef09fee33137a957c843efda4a1bc
Christopher Pietsch <cpietsch@gmail.com>
2018-02-21 17:13:29 +0100

    cleanup

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: process.env.DEPLOY ? '/nuudle-mwc/' : '/',
+    assetsPublicPath: process.env.DEPLOY ? '/BEAmeetsCOBI/' : '/',
 
     /**
      * Source Maps
