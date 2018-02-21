b8fcf6f9eaae63e57f288c753b33f6d6400d5db1
Christopher Pietsch <cpietsch@gmail.com>
2018-02-21 20:21:31 +0100

    added gyroscope orientation test for experience

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="Experience">
       <div class="content gray">
+          <div class="content-padded">
+            <p class="content-padded text-center">
+              <img class="icon" src="@/components/assets/bea-icon.png">
+            </p>
+          </div>
+          <div class="content-padded">
+            <p class="content-padded text-center">{{ beta }}</p>
+            <p class="content-padded text-center">{{ gamma }}</p>
+          </div>
 
         </div>
     </div>
@@ -19,12 +28,24 @@ export default {
       token: state => state.accessToken
     })
   },
+  data: function () {
+    return {
+      beta: 0,
+      gamma: 0
+    }
+  },
   methods: {
-
+    handleOrientation: function (event) {
+      this.gamma = event.gamma
+      this.beta = event.beta
+    }
   },
   mounted () {
       const view = COBI.parameters.state()
       console.log(view)
+
+      window.addEventListener('deviceorientation', this.handleOrientation)
+
   }
 }
 </script>
